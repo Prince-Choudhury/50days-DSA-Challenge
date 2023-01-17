c7969a89a3c085755ab15e64b6706b5c8e1ba878
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-01-17 21:31:11 +0530

    first commit

--- MaxSubArr_KadanesAlg.cpp
+++ MaxSubArr_KadanesAlg.cpp
@@ -4,8 +4,29 @@
 #include <limits.h>
 using namespace std;
 
+int getMaxSubarraySum(int arr[], int n){
+
+    int ans = INT_MIN;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+
+        ans = max(ans, sum);
+
+        if (sum < 0)
+        {
+            sum = 0;
+        }  
+    }
+    return ans;    
+}
+
 int main(){
 
-    
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    cout<<"Maximum subarray sum is : "<<getMaxSubarraySum(arr, 8);
+
     return 0;
 }
