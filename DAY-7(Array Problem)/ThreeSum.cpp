8aed19ec58fd47a2640807687eb4862645ae29e8
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-01-23 22:12:46 +0530

    commit

---
+++ DAY-7(Array Problem)/ThreeSum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include <limits.h>
+using namespace std;
+
+void threeSum(int nums[], int n){
+    
+}
+
+int main(){
+
+    int arr[] = {-1,0,1,2,-1,-4};
+
+    threeSum(arr, 6);
+
+    return 0;
+}
