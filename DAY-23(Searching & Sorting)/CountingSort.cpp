4ef5a5f3f700bb525d53c83120333fa789afd232
Prince Choudhury <99341924+Prince-Choudhury@users.noreply.github.com>
2023-02-23 14:06:10 +0530

    Counting Sort

--- DAY-23(Searching & Sorting)/CountingSort.cpp
+++ DAY-23(Searching & Sorting)/CountingSort.cpp
@@ -3,44 +3,44 @@
 #include <string.h>
 #include <vector>
 #include <limits.h>
-#define RANGE 255
 using namespace std;
 
-class solution{
-    public:
-    string countSort(string arr){
-        
-        int freq[256]={0};
-        for (int i = 0; i < arr.size(); i++)
+void countSort(int arr[], int n){
+
+    int count[8] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        count[arr[i]]++;
+    }
+
+    //Sorting
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (count[i]>0)
         {
-            freq[arr[i]]++;
+            arr[j] = i;
+            j++;
+            count[i]--;
         }
-        string s = "";
-        for (int i = 0; i < 256; i++)
-        {
-            for (int j = 0; j <freq[i] ; j++)
-            {
-                s+=char(i);
-            }
-            
-        }
-        return s;
         
     }
-};
+    
+
+}
 
 int main(){
 
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        string arr;
-        cin>>arr;
-        Solution obj;
-        cout<<obj.countSort(arr)<<endl;
+    int arr[] = {1,4,1,3,2,4,3,7};
+    int n = 8;
+    countSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
     }
+    
 
     return 0;
 }
+
+    
